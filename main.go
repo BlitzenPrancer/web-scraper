6d0c1c9550dd430da163654f452d8a3f3048a4a8
BlitzenPrancer <blitzenprancer666@gmail.com>
2021-04-16 20:53:43 +0530

    second commit

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
-import "os"
+import (
+	"log"
+	"os"
+)
 
 func main() {
 	// first thing we need is a file name
-	file := "data.csv"
+	fName := "data.csv"
 	// creating a file
-	file, err := os.Create(file)
+	file, err := os.Create(fName)
+	if err != nil {
+		// Fatalf basically prints the message and exits the program
+		log.Fatalf("File could not be created, error : %q", err)
+		return
+	}
+	defer file.Close()
 }
