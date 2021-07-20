97ab9616867e1199d71933336ca76cf1233e605a
Blitzen Prancer <76688640+BlitzenPrancer@users.noreply.github.com>
2021-07-20 11:19:18 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,19 +1,58 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
-	"os"
+	"strconv"
+
+	"github.com/gocolly/colly"
 )
 
+type Fact struct {
+	ID          int    `json:"id"`
+	Description string `json:"description"`
+}
+
 func main() {
-	// first thing we need is a file name
-	fName := "data.csv"
-	// creating a file
-	file, err := os.Create(fName)
+	allFacts := make([]Fact, 0)
+
+	collector := colly.NewCollector(
+		colly.AllowedDomains("factretriever.com", "www.factretriever.com"),
+	)
+
+	collector.OnHTML(".factsList li", func(element *colly.HTMLElement) {
+		factId, err := strconv.Atoi(element.Attr("id"))
+		if err != nil {
+			log.Println("Could not get id")
+		}
+
+		factDesc := element.Text
+
+		fact := Fact{
+			ID:          factId,
+			Description: factDesc,
+		}
+
+		allFacts = append(allFacts, fact)
+	})
+
+	collector.OnRequest(func(request *colly.Request) {
+		fmt.Println("Visiting", request.URL.String())
+	})
+
+	collector.Visit("https://www.factretriever.com/rhino-facts")
+
+	writeJSON(allFacts)
+}
+
+func writeJSON(data []Fact) {
+	file, err := json.MarshalIndent(data, "", " ")
 	if err != nil {
-		// Fatalf basically prints the message and exits the program
-		log.Fatalf("File could not be created, error : %q", err)
+		log.Println("Unable to create json file")
 		return
 	}
-	defer file.Close()
+
+	_ = ioutil.WriteFile("rhinofacts.json", file, 0644)
 }
